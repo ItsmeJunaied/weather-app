7d23fcd20b98e389e0159157699effc8cdeb3edc
Junaied <junaied526@gmail.com>
2024-05-21 18:14:28 +0600

    bug fix  added commit

--- src/api/WeatherAPI.js
+++ src/api/WeatherAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const retriveWeatherData = async (location) => {
-    const response = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=617b6d309c82448ab2c123316242005&q=${location}&aqi=no`);
+    const response = await axios.get(`https://api.weatherapi.com/v1/forecast.json?key=617b6d309c82448ab2c123316242005&q=${location}&aqi=no`);
 
     return response.data
 }
